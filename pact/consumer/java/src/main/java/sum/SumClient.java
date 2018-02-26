e6a8cb1d4e3d58a116016f1d8c404f42072c1c23
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-02-26 23:13:53 +0100

    first small calculator example

---
+++ pact/consumer/java/src/main/java/sum/SumClient.java
@@ -0,0 +1,55 @@
+package sum;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+class SumClient {
+
+	private static final MediaType JSON
+			= MediaType.parse("application/json; charset=UTF-8");
+
+	private static class Addition {
+		public List<Number> summands;
+	}
+
+	private static class Sum {
+		public Number result;
+	}
+
+	private final OkHttpClient client = new OkHttpClient();
+	private final ObjectMapper mapper = new ObjectMapper();
+	private final String url;
+
+
+	SumClient(String url) {
+		this.url = url;
+	}
+
+	public Number sum(Number summand1, Number summand2) {
+		try {
+			Addition addition = new Addition();
+			addition.summands = Arrays.asList(summand1, summand2);
+			String json = mapper.writeValueAsString(addition);
+
+			RequestBody body = RequestBody.create(JSON, json);
+			Request request = new Request.Builder()
+					.url(url + "/operations/sum")
+					.post(body)
+					.build();
+
+			Response response = client.newCall(request).execute();
+			Sum sum = mapper.readValue(response.body().string(), Sum.class);
+			return sum.result;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ pact/consumer/java/src/test/java/sum/CalculatorClientPact.java
@@ -0,0 +1,68 @@
+package sum;
+
+import au.com.dius.pact.consumer.ConsumerPactBuilder;
+import au.com.dius.pact.consumer.PactVerificationResult;
+import au.com.dius.pact.consumer.dsl.DslPart;
+import au.com.dius.pact.model.MockProviderConfig;
+import au.com.dius.pact.model.PactSpecVersion;
+import au.com.dius.pact.model.RequestResponsePact;
+import io.pactfoundation.consumer.dsl.LambdaDsl;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static au.com.dius.pact.consumer.ConsumerPactRunnerKt.runConsumerTest;
+import static au.com.dius.pact.model.MockProviderConfig.createDefault;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorClientPact {
+
+	@Test
+	public void testWithPactDSLJsonBody() {
+		System.getProperties().setProperty("pact.rootDir", "build/generated/pacts");
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json;charset=UTF-8");
+
+		DslPart requestBody = LambdaDsl.newJsonBody((o) -> {
+			o.array("summands", (s) -> {
+				s.numberValue(43).numberValue(42);
+			});
+		}).build();
+
+		DslPart responseBody = LambdaDsl.newJsonBody((o) -> {
+			o.numberValue("result", 85);
+		}).build();
+
+		RequestResponsePact pact = ConsumerPactBuilder
+				.consumer("SumService")
+				.hasPactWith("CalculatorService")
+				.given("")
+				.uponReceiving("sum two numbers")
+				.path("/operations/sum")
+				.body(requestBody)
+				.method("POST")
+				.willRespondWith()
+				.headers(headers)
+				.status(200)
+				.body(responseBody)
+				.toPact();
+
+		MockProviderConfig config = createDefault(PactSpecVersion.V3);
+		PactVerificationResult result = runConsumerTest(pact, config, mockServer -> {
+			SumClient providerHandler = new SumClient(mockServer.getUrl());
+			Number sum = providerHandler.sum(43, 42);
+			assertThat(sum).isEqualTo(85);
+		});
+
+		checkResult(result);
+	}
+
+	private void checkResult(PactVerificationResult result) {
+		if (result instanceof PactVerificationResult.Error) {
+			throw new RuntimeException(((PactVerificationResult.Error) result).getError());
+		}
+		assertEquals(PactVerificationResult.Ok.INSTANCE, result);
+	}
+}
