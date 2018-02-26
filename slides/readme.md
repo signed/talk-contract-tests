18ebbb2a99f5a13f323a3109dbc3fa3fc693b23a
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-02-26 15:41:56 +0100

    intial setup for the slides

---
+++ slides/readme.md
@@ -0,0 +1,6 @@
+run `npm install && npm present` to build the slides and start the presentation.
+
+For hints on how to edit the slides have a look at [asciidoctor-revealjs][adoc-reveal] and [asciidoc-syntax-quick-reference][quick].
+
+[adoc-reveal]: https://asciidoctor.org/docs/asciidoctor-revealjs
+[quick]: https://asciidoctor.org/docs/asciidoc-syntax-quick-reference/
---
+++ slides/scripts/scribe.js
@@ -0,0 +1,31 @@
+const asciidoctor = require('asciidoctor.js')();
+require('asciidoctor-reveal.js');
+const fs = require('fs-extra');
+const {resolve} = require('path');
+
+const base_dir = resolve(__dirname + "/..");
+const out_dir = base_dir + '/out';
+
+// Sets additional document attributes, which override equivalently-named
+// attributes defined in the document unless the value ends with @
+const own = {
+    'slides': 'slides'
+};
+
+const asciidoctor_reveal_js = {
+    'revealjsdir': base_dir + '/node_modules/reveal.js@'
+};
+
+// These are the same as for the ruby version
+// http://asciidoctor.org/docs/user-manual/#ruby-api-options
+const options = {
+    safe: 'safe',
+    backend: 'revealjs',
+    base_dir: base_dir,
+    to_dir: out_dir,
+    mkdirs: 'true',
+    attributes: Object.assign({}, own, asciidoctor_reveal_js)
+};
+
+fs.removeSync(out_dir);
+asciidoctor.convertFile('slides/presentation.adoc', options);
