68ee9abc7187bce7a7a1cf0ba713fdb009031843
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-03-02 22:49:56 +0100

    first consumer request from the operations people.

--- pact/consumer/java/src/main/java/operations/OperationsClient.java
+++ pact/consumer/java/src/main/java/operations/OperationsClient.java
@@ -1,4 +1,39 @@
 package operations;
 
-public class OperationsClient {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+
+class OperationsClient {
+
+	public static class CalculatorStatus{
+		public String status;
+	}
+
+	private final OkHttpClient client = new OkHttpClient();
+	private final ObjectMapper mapper = new ObjectMapper();
+	private final String url;
+
+	OperationsClient(String url) {
+		this.url = url;
+	}
+
+	public CalculatorStatus powerDown() {
+		try {
+			okhttp3.Request request = new okhttp3.Request.Builder()
+					.url(url + "/calculator/power")
+					.delete()
+					.build();
+			Response response = client.newCall(request).execute();
+
+
+			CalculatorStatus calculatorStatus = mapper.readValue(response.body().string(), CalculatorStatus.class);
+
+			CalculatorStatus status = new CalculatorStatus();
+			status.status = "off";
+			return calculatorStatus;
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 }
--- pact/consumer/java/src/test/java/operations/PactOperationsConsumerTest.java
+++ pact/consumer/java/src/test/java/operations/PactOperationsConsumerTest.java
@@ -1,4 +1,38 @@
 package operations;
 
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PactOperationsConsumerTest {
+
+	@Rule
+	public final PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2("CalculatorService", this);
+
+	@Pact(consumer = "OperationsService")
+	public RequestResponsePact createPact(PactDslWithProvider builder) {
+		return builder
+				.given("calculator online")
+					.uponReceiving("power down calculator")
+					.path("/calculator/power")
+					.method("DELETE")
+				.willRespondWith()
+					.status(200)
+					.body("{\"status\": \"off\"}")
+				.toPact();
+	}
+
+	@Test
+	@PactVerification
+	public void runTest() {
+		OperationsClient operationsClient = new OperationsClient(mockProvider.getUrl());
+		OperationsClient.CalculatorStatus status = operationsClient.powerDown();
+		assertThat(status.status).isEqualTo("off");
+	}
 }
