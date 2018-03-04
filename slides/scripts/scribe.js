53169c2e42c45cd86de54905903ad284e158c5a6
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-03-04 19:52:18 +0100

    use the moon theme

--- slides/scripts/scribe.js
+++ slides/scripts/scribe.js
@@ -9,22 +9,22 @@ const out_dir = base_dir + '/out';
 // Sets additional document attributes, which override equivalently-named
 // attributes defined in the document unless the value ends with @
 const own = {
-    'slides': 'slides'
+  'slides': 'slides'
 };
 
 const Transition = Object.freeze({
-    NONE: 'none',
-    FADE: 'fade',
-    SLIDE: 'slide',
-    CONVEX: 'convex',
-    CONCAVE: 'concave',
-    ZOOM: 'zoom'
+  NONE: 'none',
+  FADE: 'fade',
+  SLIDE: 'slide',
+  CONVEX: 'convex',
+  CONCAVE: 'concave',
+  ZOOM: 'zoom'
 });
 
 const TransitionSpeed = Object.freeze({
-    SLOW: 'slow',
-    DEFAULT: 'default',
-    FAST: 'fast'
+  SLOW: 'slow',
+  DEFAULT: 'default',
+  FAST: 'fast'
 });
 
 const Theme = Object.freeze({
@@ -42,23 +42,23 @@ const Theme = Object.freeze({
 
 // https://asciidoctor.org/docs/asciidoctor-revealjs/#reveal-js-options
 const asciidoctor_reveal_js = {
-    'revealjsdir': base_dir + '/node_modules/reveal.js@',
-    'revealjs_history': true,
-    'revealjs_theme': Theme.BEIGE,
-    'revealjs_transition': Transition.DEFAULT,
-    'revealjs_transitionSpeed': TransitionSpeed.DEFAULT,
-    'source-highlighter': 'highlightjs'
+  'revealjsdir': base_dir + '/node_modules/reveal.js@',
+  'revealjs_history': true,
+  'revealjs_theme': Theme.MOON,
+  'revealjs_transition': Transition.DEFAULT,
+  'revealjs_transitionSpeed': TransitionSpeed.DEFAULT,
+  'source-highlighter': 'highlightjs',
 };
 
 // These are the same as for the ruby version
 // http://asciidoctor.org/docs/user-manual/#ruby-api-options
 const options = {
-    safe: 'safe',
-    backend: 'revealjs',
-    base_dir: base_dir,
-    to_dir: out_dir,
-    mkdirs: 'true',
-    attributes: Object.assign({}, own, asciidoctor_reveal_js)
+  safe: 'safe',
+  backend: 'revealjs',
+  base_dir: base_dir,
+  to_dir: out_dir,
+  mkdirs: 'true',
+  attributes: Object.assign({}, own, asciidoctor_reveal_js)
 };
 
 fs.removeSync(out_dir);
