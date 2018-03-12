60659d076f2cbcf171ef4afab7abfdee700ecd01
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-03-12 23:02:32 +0100

    change the structure a bit

--- pact/consumer/js/tests/consumer.spec.js
+++ pact/consumer/js/tests/consumer.spec.js
@@ -23,52 +23,42 @@ describe('Calculator Pact', () => {
     spec: 2
   });
 
-  // this is the response you expect from your Provider
+  before(() => provider.setup());
+
+  after(() => provider.finalize());
+
   context('when calculator is on', () => {
-    before((done) => {
-      // (2) Start the mock server
-      provider.setup()
-      // (3) add interactions to the Mock Server, as many as required
-        .then(() => {
-          return provider.addInteraction({
-            // The 'state' field specifies a "Provider State"
-            state: 'when the calculator is on',
-            uponReceiving: 'a request to subtract two numbers',
-            withRequest: {
-              method: 'POST',
-              path: '/basic/subtraction',
-              body: {minuend: 43.0, subtrahends: [42.0]},
-              headers: {'Content-Type': 'application/json;charset=utf-8'}
-            },
-            willRespondWith: {
-              status: 200,
-              headers: {'Content-Type': 'application/json;charset=utf-8'},
-              body: {"result": 1}
-            }
-          })
-        })
-        .then(() => done())
-    });
+    before(() => provider.addInteraction({
+        // The 'state' field specifies a "Provider State"
+        state: 'when the calculator is on',
+        uponReceiving: 'a request to subtract two numbers',
+        withRequest: {
+          method: 'POST',
+          path: '/basic/subtraction',
+          body: {minuend: 43.0, subtrahends: [42.0]},
+          headers: {'Content-Type': 'application/json;charset=utf-8'}
+        },
+        willRespondWith: {
+          status: 200,
+          headers: {'Content-Type': 'application/json;charset=utf-8'},
+          body: {"result": 1}
+        }
+      })
+    );
 
     it('should subtract two numbers', () => {
       return fetch(`http://localhost:${MOCK_SERVER_PORT}/basic/subtraction`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json;charset=utf-8'},
         body: JSON.stringify({minuend: 43.0, subtrahends: [42.0]})
-      }) // TODO: Update with actual method from actual client lib
-        .then(r => r.json())
+      }).then(r => r.json())
         .then((response) => {
           expect(response.result).to.be.a('number');
           expect(response.result).to.equal(1);
-          return provider.verify();
         })
     });
 
-    // (6) write the pact file for this consumer-provider pair,
-    // and shutdown the associated mock server.
-    // You should do this only _once_ per Provider you are testing.
-    after(() => {
-      provider.finalize()
-    })
+    it("successfully verifies", () => provider.verify());
+
   })
 });
