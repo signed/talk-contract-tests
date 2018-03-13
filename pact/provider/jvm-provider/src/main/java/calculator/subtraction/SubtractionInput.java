28764bcecaf48f667a45d992d96636d064f69fba
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-03-13 23:33:07 +0100

    implement subtraction

---
+++ pact/provider/jvm-provider/src/main/java/calculator/subtraction/SubtractionInput.java
@@ -0,0 +1,9 @@
+package calculator.subtraction;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class SubtractionInput {
+    public BigDecimal minuend;
+    public List<BigDecimal> subtrahends;
+}
